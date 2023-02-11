b655643707b68b44caab5c33d1191b762481ff25
DeuceExe <deuce.exe@gmail.com>
2023-02-11 19:17:31 +0300

    Complete task 4c

--- app/src/main/java/com/example/academyhomework/homework_4.kt
+++ app/src/main/java/com/example/academyhomework/homework_4.kt
@@ -42,4 +42,46 @@ fun main() {
     println("Any \n$any")
     val (x1, x2) = listOfNumber2
     println("Destructuring $x1, $x2")
+
+    test()
+}
+
+//Задание 4c
+
+fun test() {
+    val test = mutableMapOf(
+        "Family" to "Abramov",
+        "Correct answers" to 0, 1 to "correct", 2 to "correct", 3 to "correct", 4 to "correct",
+        5 to "correct", 6 to "correct", 7 to "correct", 8 to "correct", 9 to "correct",
+        10 to "correct", 11 to "correct", 12 to "correct", 13 to "correct", 14 to "correct",
+        15 to "incorrect", 16 to "correct", 17 to "correct", 18 to "correct", 19 to "correct",
+        20 to "correct", 21 to "incorrect", 22 to "correct", 23 to "correct", 24 to "correct",
+        25 to "correct", 26 to "correct", 27 to "correct", 28 to "correct", 29 to "incorrect",
+        30 to "correct", 31 to "correct", 32 to "incorrect", 33 to "correct", 34 to "correct",
+        35 to "correct", 36 to "correct", 37 to "correct", 38 to "correct", 39 to "correct",
+        40 to "correct"
+    )
+
+    var goodCount = 0
+    var badCount = 0
+    test.forEach {
+        if (it.value == "correct") goodCount++
+        else if (it.value == "incorrect") badCount++
+    }
+
+    test += "Correct answers" to goodCount
+    test += "Incorrect answers" to badCount
+    when (goodCount) {
+        40 -> test["Grade"] = 10
+        39 -> test["Grade"] = 9
+        38 -> test["Grade"] = 8
+        in 35..37 -> test["Grade"] = 7
+        in 32..34 -> test["Grade"] = 6
+        in 29..31 -> test["Grade"] = 5
+        in 25..28 -> test["Grade"] = 4
+        in 22..24 -> test["Grade"] = 3
+        in 19..21 -> test["Grade"] = 2
+        else -> test["Grade"] = 1
+    }
+    println(test)
 }
