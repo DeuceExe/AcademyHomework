9d18a0d1787e825874edf9b6fcaff4186f054b20
DeuceExe <deuce.exe@gmail.com>
2023-02-11 18:57:57 +0300

    Complete task 3b

--- app/src/main/java/com/example/academyhomework/homework_3.kt
+++ app/src/main/java/com/example/academyhomework/homework_3.kt
@@ -1,9 +1,13 @@
 package com.example.academyhomework
 
+import kotlin.math.sqrt
+
 fun main() {
 
     factorial(7)
     println(factorial2(4))
+
+    isPrime(7)
 }
 
 //задание 3a
@@ -19,3 +23,17 @@ fun factorial(n: Int): Double {
 
 //Второй способ
 fun factorial2(n: Int): Double = if (n < 2) 1.0 else n * factorial(n - 1)
+
+//задание 3b
+
+fun isPrime(n: Int): Boolean {
+    if (n < 2) return false
+    for (m in 2..sqrt(n.toDouble()).toInt()) {
+        if (n % m == 0) {
+            println("$n is not a prime number")
+            return false
+        }
+    }
+    println("$n is a prime number")
+    return true
+}
