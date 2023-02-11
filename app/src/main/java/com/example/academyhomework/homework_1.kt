79176bb18f4047ab41332dcaa8ee52ef2d294e31
DeuceExe <deuce.exe@gmail.com>
2023-02-11 18:47:01 +0300

    Complete task 1a

---
+++ app/src/main/java/com/example/academyhomework/homework_1.kt
@@ -0,0 +1,14 @@
+package com.example.academyhomework
+
+fun main() {
+
+    //задание 1а
+
+    val text: String
+    val number = 14
+    val pointNumber: Double
+    var text2 = "Random text"
+    var number2: Int
+    var pointNumber2 = 11.2
+
+}
