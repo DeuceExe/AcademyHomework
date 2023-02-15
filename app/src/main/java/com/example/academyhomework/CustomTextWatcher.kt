415cd968546be8164615aeb6663403a530cb99de
DeuceExe <deuce.exe@gmail.com>
2023-02-15 09:48:50 +0300

    Fix bug

--- app/src/main/java/com/example/academyhomework/CustomTextWatcher.kt
+++ app/src/main/java/com/example/academyhomework/CustomTextWatcher.kt
@@ -20,8 +20,9 @@ class CustomTextWatcher(edList: Array<EditText>, bttn: Button) : TextWatcher {
     override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
     override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
     override fun afterTextChanged(s: Editable) {
+        val regexText = arrayOf(edList[0], edList[1])
         val regex = "[a-zA-Zа-яА-Я]+"
-        for (editText in edList) {
+        for (editText in regexText) {
             editText.filters = arrayOf(
                 InputFilter { source, start, end, dest, dstart, dend ->
                     if (source == "") { // for backspace
