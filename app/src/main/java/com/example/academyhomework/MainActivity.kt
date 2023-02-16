4ecd83f52646122ffcd3b5e44d42cb380eb38ab2
DeuceExe <deuce.exe@gmail.com>
2023-02-17 00:10:54 +0300

    Update master branch

--- app/src/main/java/com/example/academyhomework/MainActivity.kt
+++ app/src/main/java/com/example/academyhomework/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.academyhomework
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.academyhomework.databinding.ActivityMainBinding
 
@@ -12,5 +13,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        Log.d("life", "Create Activity")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("life", "Start Activity")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("life", "Resume Activity")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("life", "Pause Activity")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("life", "Stop Activity")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("life", "Destroy Activity")
     }
 }
