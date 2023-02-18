31542350b514264a4759cc317a8850ba68991e25
DeuceExe <deuce.exe@gmail.com>
2023-02-18 18:24:32 +0300

    Fix bug

--- app/src/main/java/com/example/academyhomework/CustomTextWatcher.kt
+++ app/src/main/java/com/example/academyhomework/CustomTextWatcher.kt
@@ -20,9 +20,10 @@ class CustomTextWatcher(edList: Array<EditText>, bttn: Button) : TextWatcher {
     override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
     override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
     override fun afterTextChanged(s: Editable) {
-        val regexText = arrayOf(edList[0], edList[1])
         val regex = "[a-zA-Zа-яА-Я]+"
-        for (editText in regexText) {
+        val regexList = arrayOf(edList[0], edList[1])
+        val numberList = arrayOf(edList[2], edList[3])
+        for (editText in regexList) {
             editText.filters = arrayOf(
                 InputFilter { source, start, end, dest, dstart, dend ->
                     if (source == "") { // for backspace
@@ -33,10 +34,12 @@ class CustomTextWatcher(edList: Array<EditText>, bttn: Button) : TextWatcher {
                     } else ""
                 }
             )
-            if (editText.text.toString().trim { it <= ' ' }.isEmpty()) {
-                button1.isEnabled = false
-                break
-            } else button1.isEnabled = true
+            for (editText2 in edList) {
+                if (editText2.text.toString().trim { it <= ' ' }.isEmpty()) {
+                    button1.isEnabled = false
+                    break
+                } else button1.isEnabled = true
+            }
         }
     }
 }
