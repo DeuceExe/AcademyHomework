871e3ac58a25aa5ced16450d16d2c4397a13193b
DeuceExe <deuce.exe@gmail.com>
2023-02-18 17:07:26 +0300

    Intent in Third Activity

--- app/src/main/java/com/example/academyhomework/SecondActivity.kt
+++ app/src/main/java/com/example/academyhomework/SecondActivity.kt
@@ -1,10 +1,18 @@
 package com.example.academyhomework
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.academyhomework.databinding.ActivitySecondBinding
+
+const val IS_MAN: String = "Is_man"
+const val BIRTHDAY: String = "Birthday"
+const val EMAIL: String = "Email"
 
 class SecondActivity : AppCompatActivity() {
 
@@ -22,6 +30,7 @@ class SecondActivity : AppCompatActivity() {
         val phone: Int = intent.getIntExtra(PHONE, 0)
         val age: Int = intent.getIntExtra(AGE, 0)
 
+
         binding.btnMain.setOnClickListener {
             val intent = Intent(applicationContext, MainActivity::class.java)
             intent.putExtra(INFO, info)
@@ -34,9 +43,38 @@ class SecondActivity : AppCompatActivity() {
 
         binding.btnThird.setOnClickListener {
             val intent = Intent(applicationContext, ThirdActivity::class.java)
+
+            fun String.isValidEmail() =
+                !isNullOrEmpty() && Patterns.EMAIL_ADDRESS.matcher(this).matches()
+            if (binding.etEmail.text.toString().isValidEmail()) startActivity(intent)
+            else {
+                val toast = Toast.makeText(applicationContext, "Incorrect email", Toast.LENGTH_LONG)
+                toast.show()
+            }
+
+            intent.putExtra(INFO, info)
+            intent.putExtra(NAME, name)
+            intent.putExtra(SURNAME, surname)
+            intent.putExtra(PHONE, phone)
+            intent.putExtra(AGE, age)
+            intent.putExtra(IS_MAN, binding.radioMan.toString())
+            intent.putExtra(BIRTHDAY, binding.etBirthday.toString())
+            intent.putExtra(EMAIL, binding.etEmail.toString())
             startActivity(intent)
         }
+
+        binding.etBirthday.setOnClickListener {
+            binding.calendar.visibility = View.VISIBLE
+            binding.layout.setBackgroundColor(Color.GRAY)
+            binding.calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+                binding.etBirthday.setText("$dayOfMonth.${month + 1}.$year")
+                binding.calendar.visibility = View.GONE
+                binding.layout.setBackgroundColor(Color.WHITE)
+            }
+        }
+
     }
+
 
     override fun onStart() {
         super.onStart()
--- app/src/main/java/com/example/academyhomework/ThirdActivity.kt
+++ app/src/main/java/com/example/academyhomework/ThirdActivity.kt
@@ -12,5 +12,14 @@ class ThirdActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityThirdBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val info: String? = intent.getStringExtra(INFO)
+        val name: String? = intent.getStringExtra(NAME)
+        val surname: String? = intent.getStringExtra(SURNAME)
+        val phone: Int = intent.getIntExtra(PHONE, 0)
+        val age: Int = intent.getIntExtra(AGE, 0)
+        val isMan: Boolean = intent.getBooleanExtra(IS_MAN, false)
+        val birthday: String? = intent.getStringExtra(BIRTHDAY)
+        val email: String? = intent.getStringExtra(EMAIL)
     }
 }
