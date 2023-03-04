b18fb22c334ec1f0843f1cbf2725d10388ca57f1
DeuceExe <deuce.exe@gmail.com>
2023-03-04 23:27:38 +0300

    Complete task with Palette (maybe)

--- app/src/main/java/com/example/academyhomework/ListAdapter.kt
+++ app/src/main/java/com/example/academyhomework/ListAdapter.kt
@@ -1,7 +1,11 @@
 package com.example.academyhomework
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.palette.graphics.Palette
 import androidx.recyclerview.widget.RecyclerView
 import com.example.academyhomework.databinding.ItemListAdapterBinding
 
@@ -9,15 +13,14 @@ class ListAdapter(
     private var dataSet: MutableList<DataList>,
     private val clickAction: (Int) -> MutableList<DataList>,
 
-    ) :
-    RecyclerView.Adapter<ListAdapter.ViewHolder>() {
+    ) : RecyclerView.Adapter<ListAdapter.ViewHolder>() {
 
     private lateinit var binding: ItemListAdapterBinding
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(viewGroup.context)
         binding = ItemListAdapterBinding.inflate(inflater, viewGroup, false)
-        return ViewHolder(binding)
+        return ViewHolder(binding, viewGroup.context)
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
@@ -36,17 +39,50 @@ class ListAdapter(
     override fun getItemCount() = dataSet.size
 
     class ViewHolder(
-        private val binding: ItemListAdapterBinding
+        private val binding: ItemListAdapterBinding, private val context: Context
     ) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: DataList) {
             with(binding) {
+                when (item.imageId) {
+                    0 -> {
+                        imagePerson.setImageResource(R.drawable.ic_baby)
+                        val bitmap1: Bitmap =
+                            BitmapFactory.decodeResource(context.resources, R.drawable.ic_baby)
+                        createPalette(bitmap1, binding)
+                    }
+                    1 -> {
+                        imagePerson.setImageResource(R.drawable.ic_schoolboy)
+                        val bitmap1: Bitmap =
+                            BitmapFactory.decodeResource(context.resources, R.drawable.ic_schoolboy)
+                        createPalette(bitmap1, binding)
+                    }
+                    2 -> {
+                        imagePerson.setImageResource(R.drawable.ic_man)
+                        val bitmap1: Bitmap =
+                            BitmapFactory.decodeResource(context.resources, R.drawable.ic_man)
+                        createPalette(bitmap1, binding)
+                    }
+                    3 -> {
+                        imagePerson.setImageResource(R.drawable.ic_elderly)
+                        val bitmap1: Bitmap =
+                            BitmapFactory.decodeResource(context.resources, R.drawable.ic_elderly)
+                        createPalette(bitmap1, binding)
+                    }
+                }
                 imagePerson.setImageResource(item.imageId)
                 tvName.text = item.name
                 tvSurname.text = item.surname
                 tvPhone.text = "+${item.phone}"
                 tvAge.text = "${item.age} years"
                 tvBirthday.text = item.birthday
+            }
+        }
+
+        private fun createPalette(bitmap: Bitmap, binding: ItemListAdapterBinding) {
+            Palette.from(bitmap).generate { palette ->
+                val defaultValue = 0x000000
+                binding.root.setBackgroundColor(palette!!.getDominantColor(defaultValue))
             }
         }
     }
